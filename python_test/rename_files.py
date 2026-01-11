e03b88dc5d92a3ec2e5059a9817e3729f6e6756b
BDX <gbadaraiah@gmail.com>
2026-01-11 09:23:18 +0530

    refactor(files): automate txt file renaming
    Renamed existing text files using a scirpt-generated naming scheme.
    Updated rename_files.py to use absolute paths and preserve extensions.

--- python_test/rename_files.py
+++ python_test/rename_files.py
@@ -1,15 +1,20 @@
+from hmac import new
 import os 
 
-os.makedirs("renamed", exist_ok=True)
 
-i = 1
 
-for i, name in enumerate(os.listdir("."), start=1):
-    if name.endswith(".txt"):
-        new_name = f"file_{i}.txt"
-        os.rename(name, os.path.join("renamed", new_name))
-        print(f"{name} -> renamed/{new_name}")
-        i += 1
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+for i, fname in enumerate(os.listdir(script_dir), start = 1):
+    if fname.endswith(".txt"):
+        modified_name = f"modified_{i}.txt"
+        old_path = os.path.join(script_dir, fname)
+        new_path = os.path.join(script_dir, modified_name)
+        os.rename(old_path, new_path)
+        print(f"{fname} | {modified_name}")
+
         
 print("**Successfully renamed the file names")
     
+
--- python_test/undo_rename.py
+++
@@ -1,7 +0,0 @@
-import os 
-
-
-for name in os.listdir("renamed"):
-    os.rename(os.path.join("renamed", name), name)
-    
-    
